3aaf351d8295cf5994536ed0b3de7b6daf28d82b
kaigaman <kaigaman@gmail.com>
2017-09-14 17:56:02 +0300

    Update class_list_fixes 2.py

--- class_list_fixes 2.py
+++ class_list_fixes 2.py
@@ -1,32 +1,31 @@
 class MyPc:
-    def __init__(self, pc_owner, pc_model, OS):
+    def __init__(self, pc_owner, pc_model, OS,pc_descrip):
         self.pc_owner = pc_owner
         self.OS = OS
         self.pc_model = pc_model
+        self.pc_descrip = pc_descrip
+ 
 
-    # There are two magic methods that do similar things but have slightly different
-    # functionality: __repr__ and __str__. What's the difference?
-    # 
-    # Create your own __str__ magic method. Change your __repr__ magic method as needed.
-    # Use both correctly from your test code.
+    def __repr__(self):
+        return " pc_owner: %s\n pc_model: %s\n OS: %s\n pc_descrip: %s\n " % (self.pc_owner, self.pc_model ,self.OS,self.pc_descrip)
 
-     def __repr__(self):
-        return " pc_owner: %s\n pc_model: %s\n OS: %s\n " % (self.pc_owner, self.pc_model ,self.OS)
+
         
 if __name__ == '__main__':
    
 
     pcs = []
-    pcs.append(MyPc('zerox', 'dell', 'windows'))
-    pcs.append(MyPc('noris', 'dell', 'linux'))
-    pcs.append(MyPc('kaiga', 'dell', 'linux'))
+    pcs.append(MyPc('zerox', 'dell', 'windows','Black. Dented. Still works fine, but it LOOKS awful!'))
+    pcs.append(MyPc('noris', 'dell', 'linux','Gray. Brand new. Still in original box!'))
+    pcs.append(MyPc('kaiga', 'dell', 'linux','black. Brand new. Still in original box!'))
 
     pc_nz = raw_input("who is the pc owner ?: ")
     pc_modelz = raw_input("What is the machine model? ")
     pc_OSz = raw_input("what is the operating system ? ")
+    pc_Descrip = raw_input("Describe the pc ? ")
 
     
-    pcz = MyPc(pc_nz,pc_modelz,pc_OSz)
+    pcz = MyPc(pc_nz,pc_modelz,pc_OSz,pc_Descrip)
 
     pcs.append(pcz)
 
